3cdbd21f344fbd23741960252657f92e62cfcfa0
Rubens Takauti <rtakauti@studiovisual.com.br>
2018-01-15 12:02:03 -0200

    Adiciona gulp php2html

---
+++ includes/php.php
@@ -0,0 +1,14 @@
+<?php
+echo <<< HTML
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+HTML;
